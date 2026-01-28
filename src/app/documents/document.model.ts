86ae5d4da7af9c0f8b1de80ccf52aa20d6284a71
ealbanca <ealbanca@byui.edu>
2026-01-27 22:08:07 -0700

    Created new components and created the document model ts file

---
+++ src/app/documents/document.model.ts
@@ -0,0 +1,11 @@
+// This is the model class for the Document
+export class Document{
+
+    constructor(public id: number, 
+        public name: string, 
+        public description: string, 
+        public url: string,
+        public children: Document[] = []){
+    }
+
+}
