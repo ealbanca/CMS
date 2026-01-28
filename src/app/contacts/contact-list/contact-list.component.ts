5dd207b55dca089b11f310934a9c708790f3d08c
ealbanca <ealbanca@byui.edu>
2026-01-27 21:27:43 -0700

    added the click event on contact-item

--- src/app/contacts/contact-list/contact-list.component.ts
+++ src/app/contacts/contact-list/contact-list.component.ts
@@ -7,7 +7,7 @@ import { Contact } from '../contact.model';
   styleUrl: './contact-list.component.css'
 })
 export class ContactListComponent implements OnInit {
-  @Output() contactWasSelected = new EventEmitter<Contact>();
+  @Output() selectedContactEvent = new EventEmitter<Contact>();
   contacts: Contact[] = [
     new Contact(1, 'R. Kent Jackson', 'jacksonk@byui.eddu', '208-496-3771', '../../assets/images/jacksonk.jpg', ),
     new Contact(2, 'Rex Barzee', 'barzeer@byui.edu', '208-496-3768', '../../assets/images/barzeer.jpg',),
@@ -20,8 +20,8 @@ export class ContactListComponent implements OnInit {
 
   }
 
-  onContactSelected(contact: Contact) {
-    this.contactWasSelected.emit(contact);
+  onSelected(contact: Contact) {
+    this.selectedContactEvent.emit(contact);
   }
 
 }
