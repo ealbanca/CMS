3d966336703456d0f566630897965dea91a3190e
ealbanca <ealbanca@byui.edu>
2026-01-30 17:44:01 -0700

    completed the evenlistener

--- src/app/documents/document-item/document-item.component.ts
+++ src/app/documents/document-item/document-item.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit, Input } from '@angular/core';
-import { Document } from '../document.model';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import  { Document } from '../document.model';
 
 @Component({
   selector: 'cms-document-item',
@@ -8,8 +8,13 @@ import { Document } from '../document.model';
 })
 export class DocumentItemComponent implements OnInit {
   @Input() document: Document;
+  @Output() documentSelected = new EventEmitter<void>();
   constructor() { }
 
   ngOnInit(){
+  }
+
+  onSelectedDocument() {
+    this.documentSelected.emit();
   }
 }
--- src/app/documents/document-list/document-list.component.ts
+++ src/app/documents/document-list/document-list.component.ts
@@ -9,11 +9,11 @@ import { Document } from '../document.model';
 export class DocumentListComponent implements OnInit {
   @Output() selectedDocumentEvent = new EventEmitter<Document>();
   documents: Document[] = [
-    new Document(1, 'First Document', 'Fisrt sample document.', 'http://example.com/first.pdf', []),
-    new Document(2, 'Second Document', 'Second sample document.', 'http://example.com/second.pdf', []),
-    new Document(3, 'Third Document', 'Third sample document.', 'http://example.com/third.pdf', []),
-    new Document(4, 'Fourth Document', 'Fourth sample document.', 'http://example.com/fourth.pdf', []),
-    new Document(5, 'Fifth Document', 'Fifth sample document.', 'http://example.com/fifth.pdf', [])
+    new Document(1, 'First Document', 'Fisrt sample document.', 'http://example.com/first.pdf',),
+    new Document(2, 'Second Document', 'Second sample document.', 'http://example.com/second.pdf',),
+    new Document(3, 'Third Document', 'Third sample document.', 'http://example.com/third.pdf',),
+    new Document(4, 'Fourth Document', 'Fourth sample document.', 'http://example.com/fourth.pdf',),
+    new Document(5, 'Fifth Document', 'Fifth sample document.', 'http://example.com/fifth.pdf',)
 ];
 
   constructor() {
