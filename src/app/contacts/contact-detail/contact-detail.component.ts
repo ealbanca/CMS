3cc07d26f3a562f24e030a9acdb206e9b4078478
ealbanca <ealbanca@byui.edu>
2026-02-17 21:43:13 -0700

    Implemented the contact edit button

--- src/app/contacts/contact-detail/contact-detail.component.ts
+++ src/app/contacts/contact-detail/contact-detail.component.ts
@@ -1,17 +1,31 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { ActivatedRoute, Router, Params } from '@angular/router';
+
 import { Contact } from '../contact.model';
+import { ContactService } from '../contact.service';
+
 
 @Component({
   selector: 'cms-contact-detail',
   templateUrl: './contact-detail.component.html',
-  styleUrl: './contact-detail.component.css'
+  styleUrls: ['./contact-detail.component.css']
 })
 export class ContactDetailComponent implements OnInit {
-  @Input() contact: Contact;
+  contact: Contact | undefined;
+  id: string;
 
 
-  constructor() { }
+  constructor( private contactService: ContactService,
+    private route : ActivatedRoute,
+    private router : Router,
+  ) { }
 
   ngOnInit(){
+    this.route.params.subscribe(
+      (params: Params) => {
+        this.id = params['id'];
+        this.contact = this.contactService.getContact(this.id);
+      }
+    );
   }
 }
