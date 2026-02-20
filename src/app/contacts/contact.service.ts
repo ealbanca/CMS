e11edd4b1747cb6c4d7dbfa0537aab8e4761018e
ealbanca <ealbanca@byui.edu>
2026-02-19 21:04:42 -0700

    Create Functions to Add, Update and Delete Contacts

--- src/app/contacts/contact.service.ts
+++ src/app/contacts/contact.service.ts
@@ -13,10 +13,12 @@ export class ContactService {
     contactSelectedEvent = new EventEmitter<Contact>();
     contacts: Contact[] = [];
     contactChangedEvent = new EventEmitter<Contact[]>();
+    maxContactId: number;
 
 
     constructor() {
         this.contacts = MOCKCONTACTS; // Initialize with mock data from file
+        this.maxContactId = this.getMaxId();
     }
 
     getContacts(): Contact[] {
@@ -26,15 +28,51 @@ export class ContactService {
         return this.contacts.find(contact => contact.id === id);
     }
 
-    deleteContact(contact: Contact){
-        if(!contact){
+    getMaxId(): number {
+        let maxId = 0;
+        this.contacts.forEach(contact => {
+            const currentId = parseInt(contact.id);
+            if (currentId > maxId) {
+                maxId = currentId;
+            }
+        });
+        return maxId;
+    }
+
+    addContact(newContact: Contact) {
+        if (!newContact || newContact === undefined) {
+            return;
+        }
+        this.maxContactId++;
+        newContact.id = this.maxContactId.toString();
+        this.contacts.push(newContact);
+        const contactsListClone = this.contacts.slice();
+        this.contactListChangedEvent.next(contactsListClone);
+    }
+
+    updateContact(originalContact: Contact, newContact: Contact) {
+        if (!originalContact || !newContact || originalContact === undefined || newContact === undefined) {
+            return;
+        }
+        const pos = this.contacts.indexOf(originalContact);
+        if (pos < 0) {
+            return;
+        }
+        newContact.id = originalContact.id;
+        this.contacts[pos] = newContact;
+        const contactsListClone = this.contacts.slice();
+        this.contactListChangedEvent.next(contactsListClone);
+    }
+    deleteContact(contact: Contact) {
+        if (!contact || contact === undefined) {
             return;
         }
         const pos = this.contacts.indexOf(contact);
-        if(pos < 0){
+        if (pos < 0) {
             return;
         }
         this.contacts.splice(pos, 1);
-        this.contactChangedEvent.emit(this.contacts.slice());
+        const contactsListClone = this.contacts.slice();
+        this.contactListChangedEvent.next(contactsListClone);
     }
 }
