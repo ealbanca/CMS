ea5ebffe881760f9799b876eb9cc6219d72804a9
ealbanca <ealbanca@byui.edu>
2026-03-31 21:34:35 -0600

    Fixed fetching messages

--- src/app/messages/message-list/message-list.component.ts
+++ src/app/messages/message-list/message-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Message } from '../message.model';
 import { MessageService } from '../message.service';
 import { Subscription } from 'rxjs';
@@ -8,26 +8,32 @@ import { Subscription } from 'rxjs';
   templateUrl: './message-list.component.html',
   styleUrl: './message-list.component.css'
 })
-export class MessageListComponent implements OnInit {
+export class MessageListComponent implements OnInit, OnDestroy {
   messages: Message[] = [];
   private subscription: Subscription;
+  private changeSub: Subscription;
 
   constructor(private messageService: MessageService) {}
 
   ngOnInit() {
-    this.subscription = this.messageService.getMessages().subscribe((response: any) => {
-      // If backend returns { messages: [...] }
-      this.messages = response.messages ? response.messages : response;
+    // Subscribe to changes in the message list
+    this.changeSub = this.messageService.messageChangedEvent.subscribe((messages: Message[]) => {
+      this.messages = messages;
     });
-  }
-
-  onAddMessage(message: Message) {
-    this.messages.push(message);
+    // Fetch the initial list from the backend
+    this.subscription = this.messageService.getMessages().subscribe((response: any) => {
+      this.messages = response.messages ? response.messages : response;
+      this.messageService['messages'] = this.messages;
+      this.messageService.messageChangedEvent.emit(this.messages.slice());
+    });
   }
 
   ngOnDestroy() {
     if (this.subscription) {
       this.subscription.unsubscribe();
+    }
+    if (this.changeSub) {
+      this.changeSub.unsubscribe();
     }
   }
 }
